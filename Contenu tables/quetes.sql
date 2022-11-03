f3e79037a6935416606a8fed6bab0689003fba56
simon TAPIAU <simtapiau@live.fr>
2022-11-03 11:32:50 +0100

    tables mise sous la bonne syntaxe pour être INSERT dans la base de données

---
+++ Contenu tables/quetes.sql
@@ -0,0 +1,7 @@
+('','Bonne brune','Mr Ohbar vous a demandé de faire de la bière brune'),
+('avoir la batte ail','','Un vampire terrorise les habitants de la ville (Iggy Pope)'),
+('','','Bernard Tappir vous demande de ramener une boubourse volée par les nains'),
+('','Victoria secret','Victoria-Big-B vous demande d''aller chercher ses objets fétiches chez la reine Bours-La'),
+('','Un carrosse dans le tunnel','Die Anna se sent lente, elle vous demande d''aller chercher le bipbip ');
+
+
