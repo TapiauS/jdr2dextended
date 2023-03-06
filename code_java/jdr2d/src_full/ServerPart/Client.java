e38e11998fdd194b424e4127e060bbeb315d190a
TapiauS <simtapiau@live.fr>
2023-03-06 20:59:33 +0100

    debut du travail sur la partie server

--- code_java/jdr2d/src_full/ServerPart/Client.java
+++ code_java/jdr2d/src_full/ServerPart/Client.java
@@ -1,6 +1,8 @@
 package ServerPart;
 
+import Control.ConnexionInput;
 import Control.InputType;
+import DAO.PersonnageDAO;
 import DAO.UtilisateurDAO;
 import jdr2dcore.Direction;
 import jdr2dcore.Personnage;
@@ -12,6 +14,8 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.sql.SQLDataException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Client extends Thread{
 
@@ -42,70 +46,213 @@ public class Client extends Thread{
     private void connect() throws IOException, ClassNotFoundException {
         String pseudo;
         String mdp;
-        while (util==null) {
-            pseudo= (String) input.readObject();
-            mdp=(String) input.readObject();
-            try {
-                util=UtilisateurDAO.connectcompte(pseudo, mdp);
-                output.writeObject(true);
-            } catch (SQLException e) {
-                if (e instanceof SQLDataException) {
-                    output.writeObject(false);
+        ConnexionInput conn=null;
+        while (util==null&&conn!=ConnexionInput.QUIT) {
+            conn=(ConnexionInput) input.readObject();
+            switch (conn) {
+                case CONNEXION -> {
+                    pseudo = (String) input.readObject();
+                    mdp = (String) input.readObject();
+                    try {
+                        util = UtilisateurDAO.connectcompte(pseudo, mdp);
+                        output.writeObject(true);
+                    } catch (SQLException e) {
+                        if (e instanceof SQLDataException) {
+                            output.writeObject(false);
+                        }
+                    }
                 }
+                case CREATION -> create();
+                case QUIT -> connected=false;
             }
         }
 
     }
 
-    private void create(){
+    private void create() throws IOException, ClassNotFoundException {
+        String pseudo="";
+        String mdp="";
+        ConnexionInput conn = null;
+        String mail="";
+        boolean success = false;
+        while (!success) {
+            conn = (ConnexionInput) input.readObject();
+            switch (conn) {
+                case VALIDCHOICE -> {
+                    String teste_pseudo;
+                    try {
+                        teste_pseudo = (String) input.readObject();
+                        success = UtilisateurDAO.checkpseudo(teste_pseudo);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (success)
+                        pseudo = teste_pseudo;
+                    output.writeObject(success);
+                }
+                case CONNEXION -> {
+                    connect();
+                    return;
+                }
+                case QUIT -> {
+                    connected = false;
+                    return;
+                }
+            }
+        }
+        success = false;
+        while (!success) {
+            conn = (ConnexionInput) input.readObject();
+            switch (conn) {
+                case VALIDCHOICE -> {
+                    String teste_mdp;
+                    try {
+                        teste_mdp = (String) input.readObject();
+                        success = UtilisateurDAO.checkmdp(teste_mdp);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (success)
+                        mdp = teste_mdp;
+                    output.writeObject(success);
+                }
+                case CONNEXION -> {
+                    connect();
+                    return;
+                }
+                case QUIT -> {
+                    connected = false;
+                    return;
+                }
+            }
+        }
+        success = false;
+        while (!success) {
+            conn = (ConnexionInput) input.readObject();
+            switch (conn) {
+                case VALIDCHOICE -> {
+                    String teste_mail;
+                    try {
+                        teste_mail = (String) input.readObject();
+                        success = UtilisateurDAO.checkmail(teste_mail);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (success)
+                        mail = teste_mail;
+                    output.writeObject(success);
+                }
+                case CONNEXION -> {
+                    connect();
+                    return;
+                }
+                case QUIT -> {
+                    connected = false;
+                    return;
+                }
+            }
+        }
+        try {
+            UtilisateurDAO.createcompte(pseudo,mdp,mail);
+            util=UtilisateurDAO.connectcompte(pseudo,mdp);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void pick(){
+        try {
+            Hashtable<String,Integer> display=UtilisateurDAO.displaypersonnage(util);
+            output.writeObject(display);
+            ConnexionInput conn= (ConnexionInput) input.readObject();
+            switch (conn){
+                case PICKCHAR -> {
+                    String nomperso= (String) input.readObject();
+                    avatar= PersonnageDAO.getchar(display.get(nomperso));
+                }
+                case CREATION -> createchar();
+                case QUIT -> connected=false;
+            }
+        } catch (SQLException | IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createchar(){
+        String charname="";
+        boolean success=false;
+        ConnexionInput conn;
+        while (!success){
+            try {
+                conn= (ConnexionInput) input.readObject();
+
+            } catch (IOException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void pickpicture(){
 
     }
 
     public void run(){
         //TODO sequence de connection
-        boolean connect;
+        ConnexionInput connect;
         try {
-            connect = input.readBoolean();
-            if (connect) {
-                connect();
-            } else {
-                create();
-            }
-        }
+            connect = (ConnexionInput) input.readObject();
+            switch (connect) {
+                case CONNEXION -> connect();
 
+                case CREATION -> create();
 
-        catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+                case QUIT -> connected=false;
+                }
+            } catch (IOException | ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
         }
-        MapPool.addClient(this);
-        InputType inputType ;
-        do {
+        if(connected){
             try {
-                inputType = (InputType) input.readObject();
+                connect=(ConnexionInput) input.readObject();
+                switch (connect){
+                    case PICKCHAR -> pick();
+                    case CREATECHAR -> createchar();
+                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
-        switch (inputType){
-            case MOUVNORD -> avatar.depl(Direction.NORD);
-            case MOUVWEST -> avatar.depl(Direction.OUEST);
-            case MOUVEAST -> avatar.depl(Direction.SUD);
-            case MOUVSOUTH -> avatar.depl(Direction.SUD);
-
+        }
+        if(connected) {
+            MapPool.addClient(this);
+            InputType inputType ;
+            do {
+                try {
+                    inputType = (InputType) input.readObject();
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                switch (inputType) {
+                    case MOUVNORD -> avatar.depl(Direction.NORD);
+                    case MOUVWEST -> avatar.depl(Direction.OUEST);
+                    case MOUVEAST -> avatar.depl(Direction.EST);
+                    case MOUVSOUTH -> avatar.depl(Direction.SUD);
+                    case QUIT -> connected=false;
             /*
             case TALK -> break;
             case FIGTH -> break;
             case PICK -> break;
             case QUEST -> break;
-            case QUIT -> break;
+
             case INVENTAIRE -> break;
             case EQUIP -> break;
             case USE -> break;
             case DROP -> break;
             */
+                }
+            } while (connected);
         }
-        }while (inputType!=InputType.QUIT&&connected);
 
     }
 
